4eb205d29b966bdd5f90a4d85846fc679f3925ec
Uri Berman <uribk@unito.io>
2020-05-25 09:32:56 -0400

    improved about

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -14,9 +14,6 @@ export function About() {
           My creator is pretty scatterbrained and often forgets what tempo he was on when practicing guitar.
           That's why he created me, I let him remember where he was on each song!
         </p>
-        <p>
-          He also wanted to learn ReactJS, and for some reason he thought I'd be a good idea for that. Silly human.
-        </p>
         <a className="github-link" href="https://github.com/Urik/Clickie" target="_blank" title="Visit me on Github!">
           <p>You can visit me on Github :D !</p>
         </a>
