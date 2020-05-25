84060e937e06c8ff30606e393b927ec0dff654d8
Uri Berman <uribk@unito.io>
2020-05-24 21:45:15 -0400

    fixed about message

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -11,8 +11,8 @@ export function About() {
       <div className="text">
         <p>I'm Clickie, a free, no ads, mobile friendly metronome for casual use!</p>
         <p>
-          My creator is pretty scatterbrained and often forgets what tempo he was on when practicing guitar and
-          that's why he created me, I let him remember where he was on each song!
+          My creator is pretty scatterbrained and often forgets what tempo he was on when practicing guitar.
+          That's why he created me, I let him remember where he was on each song!
         </p>
         <p>
           He also wanted to learn ReactJS, and for some reason he thought I'd be a good idea for that. Silly human.
